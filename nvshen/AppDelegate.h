40e4f88cdc1e028f1867616ad70356a28d8d2e3a
hoolang <hoolang@gmail.com>
2015-05-08 15:25:18 +0800

    Initial Commit

---
+++ nvshen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nvshen
+//
+//  Created by hoolang on 15/5/8.
+//  Copyright (c) 2015年 Hoolang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nvshen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nvshen
+//
+//  Created by hoolang on 15/5/8.
+//  Copyright (c) 2015年 Hoolang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
